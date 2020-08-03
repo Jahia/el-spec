bc09a9d7a29402398b0b6b13d782790a12a6840b
tdraier <tdraier@jahia.com>
2020-08-03 11:16:44 +0200

    BACKLOG-14285: Added option to disable on lower case only, added more logs in warn

--- src/main/java/javax/el/ImportHandler.java
+++ src/main/java/javax/el/ImportHandler.java
@@ -116,11 +116,8 @@ public class ImportHandler {
         if (System.getProperty("javax.el.class-resolution.disable") != null) {
             return null;
         }
-
-        try {
-            Class.forName("org.jahia.osgi.LogBridge").getMethod("log", String.class, int.class, Object.class, Throwable.class).invoke(null, ImportHandler.class.getName(), 10000, "Trying to resolve class : " + name, null);
-        } catch (Exception e) {
-            // Cannot log
+        if (System.getProperty("javax.el.class-resolution.disableOnLowerCase") != null && !name.startsWith(name.toUpperCase())) {
+            return null;
         }
 
         String className = classNameMap.get(name);
@@ -136,6 +133,17 @@ public class ImportHandler {
                 return c;
             }
         }
+
+        try {
+            if (System.getProperty("javax.el.class-resolution.logStackTrace") != null) {
+                Class.forName("org.jahia.osgi.LogBridge").getMethod("log", String.class, int.class, Object.class, Throwable.class).invoke(null, ImportHandler.class.getName(), 30000, "Tried to resolve class : " + name + " , but was not found. This can have a performance impact, check your JSPs to optimize this call.", new Exception());
+            } else {
+                Class.forName("org.jahia.osgi.LogBridge").getMethod("log", String.class, int.class, Object.class, Throwable.class).invoke(null, ImportHandler.class.getName(), 30000, "Tried to resolve class : " + name + " , but was not found. This can have a performance impact, check your JSPs to optimize this call.", null);
+            }
+        } catch (Exception e) {
+            // Cannot log
+        }
+
         return null;
     }
 
